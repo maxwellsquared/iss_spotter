28254440fd14d75f027671d725fd811030f9496e
max k <maxkuhn@gmail.com>
2022-07-08 00:57:54 -0700

    finished iss spotter I, working on II

---
+++ iss.js
@@ -0,0 +1,28 @@
+const request = require('request');
+
+/**
+ * Makes a single API request to retrieve the user's IP address.
+ * Input:
+ *   - A callback (to pass back an error or the IP string)
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The IP address as a string (null if error). Example: "162.245.144.188"
+ */
+ const fetchMyIP = function(callback) { 
+
+  request("https://api.ipify.org?format=json", (error, response, body) => {
+    if (error) return callback(error, null);
+    if (response.statusCode !== 200) {
+      const msg = `Status code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+    callback(null, JSON.parse(body).ip);
+  });
+}
+
+const fetchCoordsByIP
+
+// http://ipwho.is/[IP address]
+
+module.exports = { fetchMyIP };
